1a20db8ba4225ce2185a96c0572322b7d6eea4e2
Mark Fernandes <0xMF@0xMF.com>
2020-06-30 19:34:50 -0400

    0xMF Emacs settings (also: 0xMF/dotfiles)

---
+++ lisp/pre-load-local.el
@@ -0,0 +1,8 @@
+;;----------------------------------------------------------------------------
+;; personalize purcell's emacs.d/init.el settings for my setup
+;;----------------------------------------------------------------------------
+
+;; disable warning message when starting up Emacs via crouton
+(setq exec-path-from-shell-check-startup-files nil)
+
+;;; pre-load-local.el ends here
