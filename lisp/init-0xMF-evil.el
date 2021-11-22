8e01093976264b97bc1647107479ed94e62d9c01
Mark Fernandes <0xMF@0xMF.com>
2021-11-22 18:34:15 -0500

    start setting up shortcuts for slime that are rational to me

--- lisp/init-0xMF-evil.el
+++ lisp/init-0xMF-evil.el
@@ -207,6 +207,14 @@
       scroll-conservatively 9999
       scroll-step 1)
 
+(defun 0xMF/settings/slime ()
+  "My slime settings."
+  (interactive)
+  (turn-on-evil-mode)
+  (dolist (map (list slime-repl-mode-map))
+    (local-unset-key (kbd  "C-<return>"))
+    (define-key map (kbd "C-<return>") 'slime-repl-newline-and-indent)))
+
 (defun 0xMF/settings/vi ()
   "My Vi settings."
   ;; jump j/k always even in visual mode
