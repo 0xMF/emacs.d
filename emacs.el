c22ca3c047a3996e2b3e04b1599b8ae015e466eb
Mark Fernandes <0xMF@0xMF.com>
2022-09-29 07:43:49 -0400

    package cleanup

--- emacs.el
+++ emacs.el
@@ -32,38 +32,6 @@
 
 (load "~/.emacs.d/init")
 
-(quote (package-selected-packages '(add-node-modules-path
-                                    aggressive-indent alert anaconda-mode anzu auto-compile avy beacon browse-at-remote
-                                    browse-kill-ring bug-reference-github bundler cask-mode cider cl-lib-highlight cl-libify
-                                    cljsbuild-mode clojure-mode coffee-mode color-theme-sanityinc-solarized
-                                    color-theme-sanityinc-tomorrow command-log-mode company company-anaconda company-nixos-options
-                                    company-php company-quickhelp company-terraform consult consult-flycheck counsel crontab-mode
-                                    css-eldoc csv-mode daemons dante default-text-scale dhall-mode diff-hl diminish dimmer diredfl
-                                    disable-mouse docker docker-compose-mode dockerfile-mode dotenv-mode dune dune-format elein
-                                    elisp-slime-nav elm-mode elm-test-runner embark embark-consult envrc eradio erlang
-                                    evil-collection evil-magit exec-path-from-shell expand-region fill-column-indicator flycheck
-                                    flycheck-clojure flycheck-color-mode-line flycheck-elm flycheck-ledger flycheck-nim
-                                    flycheck-package flycheck-relint flycheck-rust forge general fullframe git-blamed git-commit
-                                    git-timemachine gitconfig-mode github-clone github-review gitignore-mode gnu-elpa-keyring-update
-                                    gnuplot go-mode goto-line-preview haml-mode haskell-mode hide-mode-line
-                                    highlight-escape-sequences highlight-quoted hindent htmlize httprepl ibuffer-projectile
-                                    ibuffer-vc immortal-scratch inf-ruby info-colors ipretty ivy ivy-rich ivy-xref j-mode
-                                    js-comint js2-mode json-mode ledger-mode list-unicode-display lua-mode macrostep magit
-                                    magit-todos marginalia markdown-mode merlin merlin-company merlin-eldoc mmm-mode
-                                    mode-line-bell move-dup multiple-cursors nginx-mode nim-mode nix-buffer nix-mode nix-sandbox
-                                    nixos-options nixpkgs-fmt orderless org org-beautify-theme org-bullets org-caldav
-                                    org-cliplink org-gcal org-gtd org-noter-pdftools org-pomodoro org-present
-                                    org-static-blog origami ox-asciidoc page-break-lines paredit php-mode pip-requirements
-                                    powerline prettier-js projectile projectile-rails psc-ide psci purescript-mode racer
-                                    racket-mode rainbow-delimiters rainbow-mode reformatter regex-tool restclient robe rspec-mode
-                                    ruby-compilation ruby-hash-syntax rust-mode sass-mode scratch selectrum selectrum-prescient
-                                    seq session shfmt skewer-less skewer-mode slime slime-company smart-mode-line
-                                    smart-mode-line-powerline-theme smarty-mode smex sqlformat ssh-agency sudo-edit swiper
-                                    switch-window symbol-overlay tagedit terraform-mode textile-mode toml-mode tuareg
-                                    typescript-mode undo-fu unfill uptimes use-package vc-darcs vertico vlf wgrep which-key
-                                    whitespace-cleanup-mode whole-line-or-region windswap writeroom-mode yafolding yagist
-                                    yaml-mode yard-mode yari)))
-
 ;; Local Variables:
 ;; coding: utf-8
 ;; no-byte-compile: t
