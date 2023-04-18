3dd667a98499e1700e464e6e6e8d8187f5f51930
Mark Fernandes <0xMF@0xMF.com>
2023-04-18 10:48:00 -0400

    edit out unused

--- lisp/init-0xMF-evil.el
+++ lisp/init-0xMF-evil.el
@@ -178,7 +178,6 @@
                     "p" '0xMF/settings/theme
                     "P" '0xMF/settings/show-cursor-toggle ;;pdf-view ;;start-slideshw ;;'org-present
                     "q" 'toggle-truncate-lines ;;'visual-line-mode ;;fill-paragraph
-                    "r" '0xMF/reset
                     "R" 'file-reload ;;'undo-tree-redo
                     "s" '0xMF/startup
                     "T" 'org-set-tags
