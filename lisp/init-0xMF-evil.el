7844109f1b6d90b014172f18d425be54000dff74
Mark Fernandes <0xMF@0xMF.com>
2024-10-16 11:16:48 -0400

    reconcile theme switching with my vim keybindings

--- lisp/init-0xMF-evil.el
+++ lisp/init-0xMF-evil.el
@@ -188,8 +188,8 @@
                     "a" '0xMF/settings/orgmode-emphasis-markers-toggle
                     "A" 'org-agenda
                     "B" 'switch-to-buffer
-                    "c" 'comment-region
-                    "C" 'org-capture
+                    "c" '0xMF/settings/theme
+                    "C" 'comment-region ;; 'org-capture
                     "d" 'insdate-insert-current-date
                     "D" 'org-agenda-list
                     "e" 'eval-last-sexp
