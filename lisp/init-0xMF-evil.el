4297c6ab3bee782edfcef3160a3db8f2df592dfe
Mark Fernandes <0xMF@0xMF.com>
2024-10-21 11:45:21 -0400

    handle edge case for cursor-column-toggle

--- lisp/init-0xMF-evil.el
+++ lisp/init-0xMF-evil.el
@@ -71,10 +71,11 @@
 (defun 0xMF/settings/cursor-column-toggle()
   (interactive)
   "Show and follow cursor column."
-  (if (string= (face-background 'default) "#f5f5dc")
-      (set-face-background vline-face "light steel blue")
-    (set-face-background vline-face "firebrick"))
-  (vline-mode 'toggle))
+  (if (boundp vline-face)
+      (if (string= (face-background 'default) "#f5f5dc")
+          (set-face-background vline-face "light steel blue")
+        (set-face-background vline-face "firebrick"))
+    (vline-mode 'toggle)))
 
 (defun 0xMF/settings/cursor-toggle ()
   "Toggle showing cursor."
